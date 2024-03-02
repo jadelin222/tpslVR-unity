d19220e7cdeb8ebd6c1826e71bf0ceab47b7bb86
yong <109802312+jadelin222@users.noreply.github.com>
2024-03-02 00:10:35 +0000

    add the ability to brush teeth

--- test project/Assets/Scripts/Consumer.cs
+++ test project/Assets/Scripts/Consumer.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Consumer : MonoBehaviour
 {
     Collider _collider;
+    public AudioSource brushingSound;
+
     void Start()
     {
         _collider = GetComponent<Collider>();
@@ -17,7 +21,55 @@ public class Consumer : MonoBehaviour
         {
             consumable.Consume();
         }
+        else if (other.CompareTag("Toothbrush")) // Check if the collider is tagged as "Toothbrush"
+        {
+            StartBrushing();
+            if (!brushingSound.isPlaying)
+            {
+                brushingSound.Play(); 
+            }
+        }
 
+    }
+
+    void StartBrushing()
+    {
+        // Assume using the right hand controller for this example
+        var devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller, devices);
+
+        foreach (var device in devices)
+        {
+            HapticCapabilities capabilities;
+            if (device.TryGetHapticCapabilities(out capabilities) && capabilities.supportsImpulse)
+            {
+                uint channel = 0;
+                float amplitude = 0.5f; // Vibration strength from 0 to 1
+                float duration = 1f; // Vibration duration in seconds
+                device.SendHapticImpulse(channel, amplitude, duration);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Toothbrush")) // When the toothbrush exits the mouth
+        {
+            StopBrushing();
+            brushingSound.Stop();
+        }
+    }
+
+    void StopBrushing()
+    {
+        // Stop the haptic feedback when the toothbrush is removed
+        var devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller, devices);
+
+        foreach (var device in devices)
+        {
+            device.StopHaptics();
+        }
     }
 
 }
